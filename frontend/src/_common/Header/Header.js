03238cb239384cca7a6b80e45ca1252670b3f30c
iwaduarte <iwaduarte@gmail.com>
2020-04-17 15:18:43 -0300

    Header.js css, logo and layout implemented

--- frontend/src/_common/Header/Header.js
+++ frontend/src/_common/Header/Header.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import logo from "../../logo.svg";
 
-const Header = ()=> <>
-    <header className="App-header">
+// import logo from "../../logo.svg";
+import styled from "styled-components";
 
-    </header>
+const HeaderBox = styled.div`
+display: flex;
+align-items: center;
+padding-left: 1vw;
+background-color: #121212;
+color: #FFF;
+min-height: calc(60px + 3vh);
+`;
+const Logo = styled.div`
+font-size: 4rem;
+font-family: Bangers;
+color: #f5c517;
+`;
+const MenuBar = styled.div`
+display: flex;
+flex-grow: 1;
+justify-content: space-evenly;
+font-size: 1.8rem;
+font-family: Arial;
+cursor: pointer;
+// margin: auto;
+
+`;
+const MenuItem = styled.div`
+&:hover{
+color: #f5c517;
+}
+
+`;
+const Header = () => <>
+    <HeaderBox className="">
+        <Logo>
+            Movie Me
+        </Logo>
+        <MenuBar>
+            <MenuItem> Recommendations</MenuItem>
+            <MenuItem> Liked</MenuItem>
+            <MenuItem> Not Liked</MenuItem>
+        </MenuBar>
+    </HeaderBox>
 </>;
 
 
