a66ee99f40a75251c3af7dfad829a15bd330ff4c
iwaduarte <iwaduarte@gmail.com>
2020-04-18 16:12:24 -0300

    Restructuring App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './App.css';
-import Home from './Home/Home';
+import {BrowserRouter} from 'react-router-dom'
+import Header from './_common/Header/Header';
+import Main from './_common/Main/Main';
+import Footer from './_common/Footer/Footer';
 
-const App = () => <Home/>;
+
+const App = () => <BrowserRouter>
+    <Header/>
+    <Main/>
+    <Footer/>
+</BrowserRouter>;
 
 export default App;
