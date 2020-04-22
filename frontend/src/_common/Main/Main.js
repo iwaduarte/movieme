723bd76fa350d00d154c425b58f9457198170e4b
iwaduarte <iwaduarte@gmail.com>
2020-04-22 19:57:35 -0300

    Rate function created

--- frontend/src/_common/Main/Main.js
+++ frontend/src/_common/Main/Main.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from "../../Home/Home";
 import Watched from "../../Watched/Watched";
 import Quiz from "../../Quiz/Quiz";
 
+const moviesArr = [
+    {
+        Title: 'Guardians of The Galaxy (2009)',
+        Year: '2009',
+        Genre: '',
+        description: 'Bunch of criminals get together to save galaxy from another criminals',
+        imdbRating: '8',
+        Poster: '/guardians_img.jpg',
+    },
+    {
+        title: '',
+        description: '',
+        imdbRating: '',
+        image: '',
+    },
+    {
+        title: '',
+        description: '',
+        imdbRating: '',
+        image: '',
+    },
+    {
+        title: '',
+        description: '',
+        imdbRating: '',
+        image: '',
+    },
+    {
+        title: '',
+        description: '',
+        imdbRating: '',
+        image: '',
+    },
+    {
+        title: '',
+        description: '',
+        imdbRating: '',
+        image: '',
+    }
+];
 
-const Main = props => <Switch>
+
+const Main = props => {
+    const [movies, setMovies] = useState(moviesArr);
+    const [moviesWatched, setMoviesWatched] = useState([]);
+
+    const rate = useCallback((rate) => {
+        if (rate === 'liked')
+
+
+            console.log('liked');
+        else if (rate === 'disliked')
+            console.log('disliked');
+        else console.log('neutral');
+    },[]);
+
+    return <Switch>
         <Route exact path="/">
-            <Home/>
+            <Home rate={rate} movies={movies} />
         </Route>
         <Route exact path="/watched">
             <Watched/>
@@ -16,4 +71,5 @@ const Main = props => <Switch>
             <Quiz/>
         </Route>
     </Switch>;
+}
 export default Main;
