dd2d545fc8912f6668d1b95c2201d9787a6add65
iwaduarte <iwaduarte@gmail.com>
2020-04-22 19:57:21 -0300

    CRUD custom Hook

---
+++ frontend/src/_common/myHooks/useAPI.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
+const handleGet = () => {
+
+    return axios.get()
+        .then()
+        .catch();
+};
+const handleCreate = () => {
+
+};
+const handleUpdate = () => {
+
+};
+
+const handleDelete = () => {
+
+};
+
+
+
+const useApi = () => {
+
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+
+        handleGet();
+
+    }, [handleGet]);
+
+
+};
+
+export default useApi;
