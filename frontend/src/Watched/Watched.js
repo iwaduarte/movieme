e53a53166bd371318c52d9c705972e25b02c6e4e
iwaduarte <iwaduarte@gmail.com>
2020-05-19 04:51:37 -0300

    Watched page updated

--- frontend/src/Watched/Watched.js
+++ frontend/src/Watched/Watched.js
@@ -23,17 +23,19 @@ const Watched = ({movies}) => <WatchedBox>
     <Title> Liked (10): </Title>
     <WatchedSection name="Liked">
         {movies['liked'] && movies['liked'].length && movies['liked']
-            .map((movie, index) => <MovieCard movie={movie} key={index} rate={() => console.log('remove')}/>)}
+            .map((movie, index) => <MovieCard rated movie={movie} key={index} rate={() => console.log('remove')}/>)}
     </WatchedSection>
     <Title> Do not like (20):</Title>
     <WatchedSection name="Disliked">
         {movies['disliked'] && movies['disliked'].length && movies['disliked'].map((movie, index) => <MovieCard
+            rated
             movie={movie} key={index}
             rate={() => console.log('remove')}/>)}
     </WatchedSection>
     <Title> No impressions (3):</Title>
     <WatchedSection name="No impressions">
         {movies['opinionless'] && movies['opinionless'].length && movies['opinionless'].map((movie, index) => <MovieCard
+            rated
             movie={movie} key={index}
             rate={() => console.log('remove')}/>)}
 
