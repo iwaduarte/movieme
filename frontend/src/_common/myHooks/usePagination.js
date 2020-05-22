bbbcdb45b9d8fc7e8f5f243fb3774fdc8ffb7c80
iwaduarte <iwaduarte@gmail.com>
2020-05-22 17:49:54 -0300

    Implemented use pagination (fully)
    - Provides an array containing the number of pages of that specific data
    - Prev and Next function fully functional ( circular next and previous i.e goes to the beginning when reached the end and vice-versa)
    - Goes to the specific page

--- frontend/src/_common/myHooks/usePagination.js
+++ frontend/src/_common/myHooks/usePagination.js
@@ -2,39 +2,48 @@ import React, {useEffect, useState} from 'react';
 
 const usePagination = (data, offset) => {
 
-    // const [data, setData] = useState(data);
     const [dataChunk, setDataChunk] = useState([]);
-    const [position, setPosition] = useState(offset);
+    const [position, setPosition] = useState(0);
+    const [navigation, setNavigation] = useState([]);
 
     useEffect(() => {
-        setDataChunk(data.slice(0, offset))
+        setDataChunk(data.slice(0, offset));
+        const nav = [...Array(Math.ceil(data.length / offset) + 1).keys()];
+        nav.shift();
+        setNavigation(nav);
+
     }, []);
 
-
     const nextChunk = () => {
-        if (position > data.length) {
+        // console.log(`position [${position}],offset[${offset}]`);
+        const newPosition = position + offset;
+        if (newPosition >= data.length) {
+            setPosition(0);
             setDataChunk(data.slice(0, offset));
-            setPosition(offset);
             return;
         }
-        setDataChunk(data.slice(position, position + offset));
+        setDataChunk(data.slice(newPosition, newPosition + offset));
         setPosition(prevPosition => prevPosition + offset);
     };
 
     const prevChunk = () => {
-
-        if (position - offset < 0 ) {
-            console.log('position',position);
-            const startPosition = Math.floor((data.length / offset)) * offset;
-            setDataChunk(data.slice(startPosition, data.length ));
+        // console.log(`position [${position}],offset[${offset}] prev`);
+        if (position - offset < 0) {
+            const startPosition = (Math.ceil(data.length / offset) - 1) * offset;
             setPosition(startPosition);
+            setDataChunk(data.slice(startPosition, data.length));
             return;
         }
-        setDataChunk(data.slice(position - offset, position));
         setPosition(prevPosition => prevPosition - offset);
+        setDataChunk(data.slice(position - offset, position));
     };
 
+    const goToPage = (page) => {
+        // console.log('goToPage', page);
+        setPosition((page - 1) * offset);
+        setDataChunk(data.slice((page - 1) * offset, page * offset));
+    };
 
-    return [dataChunk, nextChunk, prevChunk]
+    return [dataChunk, nextChunk, prevChunk, navigation, goToPage]
 };
 export default usePagination;
